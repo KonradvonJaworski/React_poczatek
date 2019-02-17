cf3b1256a2921288fcef931c4348361fc843ae04
Konrad Jaworski <konradx530@gmail.com>
2019-02-18 00:31:21 +0100

    pierwsze testy reacta

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// tworzenie konponenu 
+// przy instrukcja return musi sie znajdowac div w jednej lini bo inaczej JS zwraca powietrze
+// lub drugi sposob " return (" i poniezej div 
+const App = () => {
+        const ButtonTextV = { text: 'Kliknij tu' }
+        const lebelText = 'Enter name:';
+  
+
+         return (
+           <div>
+             <p> tytuł </p>
+             <label className="label" htmlFor="name">
+               {lebelText}
+             </label>
+             <input id="name" type="text" />
+             <button  style={{ backgroundColor: "red", color: "white" }} >
+               {ButtonTextV.text}
+              </button>
+              <p>  testowy tekst </p> 
+           </div>
+         );
+};
+
+// pokazanie komponentu na ekranie 
+ReactDOM.render(<App />, document.querySelector('#root'));
